f66c751e7dbc31c9ff3b7b3a4c3b2fcef5ba2e72
Andres Miranda <andres.miranda.dev@gmail.com>
2023-08-09 13:02:13 +0900

    added upload screen component and added it to confirm visit hanlder

--- screens/visitScreens/VisitsScreen.js
+++ screens/visitScreens/VisitsScreen.js
@@ -1,22 +1,27 @@
 import { Alert, StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
 
 import Screen from '../../components/Screen';
 import ScheduledVisit from '../../components/visitscreen/ScheduledVisit';
 import ConfirmVisit from '../../components/visitscreen/ConfirmVisit';
 import Reschedule from '../../components/visitscreen/Reschedule';
+import UploadScreen from '../UploadScreen';
 
 export default function VisitsScreen({ navigation }) {
+  const [showDone, setShowDone] = useState(false);
+
   const handleConfirm = () => {
+    setShowDone(true);
+  };
+
+  const handleConfirmButtonPress = () => {
     Alert.alert(
       'Confirmar cita',
       '¿Desea confirmar la cita programada para el 1 de enero?',
       [
         {
           text: 'Si',
-          onPress: () => {
-            Alert.alert('Cita para el 1 de enero ha sido confirmada.');
-          },
+          onPress: () => handleConfirm(),
         },
         { text: 'Cancelar', style: 'destructive' },
       ]
@@ -24,11 +29,16 @@ export default function VisitsScreen({ navigation }) {
   };
   return (
     <Screen>
+      <UploadScreen
+        visible={showDone}
+        onDone={() => setShowDone(false)}
+        infoText="Su cita ha sido confirmada"
+      />
       <View style={styles.ScheduledVisit}>
         <ScheduledVisit />
       </View>
       <View style={styles.actions}>
-        <ConfirmVisit onPress={handleConfirm} />
+        <ConfirmVisit onPress={handleConfirmButtonPress} />
         <Reschedule
           onPress={() => {
             navigation.navigate('rescheduleScreen');
