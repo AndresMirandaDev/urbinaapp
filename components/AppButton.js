7fd20bc382fd5abf024450ab27ed56c26371d22c
Andres Miranda <andres.miranda.dev@gmail.com>
2026-03-20 09:52:36 +0900

    se borro un import que no se usaba

--- components/AppButton.js
+++ components/AppButton.js
@@ -1,5 +1,4 @@
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import React from 'react';
 import colors from '../config/colors';
 import appStyles from '../config/styles';
 
